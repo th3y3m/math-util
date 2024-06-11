f0fec57c42eb620f25bea5e64160f7e1b50c7593
th3y3m <truongtanhuy3006@gmail.com>
2024-06-11 10:01:39 +0700

    update main code to use recursion

--- src/main/java/com/mycompany/mathutil/core/MathUtility.java
+++ src/main/java/com/mycompany/mathutil/core/MathUtility.java
@@ -10,7 +10,7 @@ package com.mycompany.mathutil.core;
  */
 public class MathUtility {
     public static long getFactorial(int n){
-        long product = 1;
+//        long product = 1;
         //update code again to see green
         
         if (n < 0 || n > 20) {
@@ -21,9 +21,18 @@ public class MathUtility {
             return 1;
         }
         
-        for (int i = 1; i <= n; i++) {
-            product *= i;
-        }
-        return product;
+        return n * getFactorial(n-1);
+        //recursion - đệ quy - gọi lại chính mình với 1 quy mô khác
+        
+//        for (int i = 1; i <= n; i++) {
+//            product *= i;
+//        }
+//        return product;
     }
 }
+//5!    = 1.2.3.4.5
+//      = 5 . 4!
+//4!    = 1.2.3.4
+//      4 . 3!
+//3!    = 3 . 2!
+//2!    = 2 . 1!
